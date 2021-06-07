4ad6650583a938f81c9c9b820bbf900fa62ca56e
hnnazm <mhannan143@gmail.com>
2021-06-07 21:50:04 +0800

    Update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import store from '@/store/index'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -8,22 +10,51 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    redirect: { name: 'ModelType' },
+    children: [
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About.vue')
+      },
+      {
+        path: '/devicemodel',
+        name: 'DeviceModel',
+        component: () => import('../views/DeviceModel.vue')
+      },
+      {
+        path: '/modeltype',
+        name: 'ModelType',
+        component: () => import('../views/ModelType.vue')
+      },
+      {
+        path: '/user',
+        name: 'User',
+        component: () => import('../views/User.vue')
+      },
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    beforeEnter: function(to, from, next) {
+      if (store.state.isAuthenticated) next({ name: 'Home' })
+      else next()
+    }
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !store.state.isAuthenticated) next({ name: 'Login' })
+  else next()
 })
 
 export default router
